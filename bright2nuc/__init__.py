3bfa5156e0e0efe1cdb6e3c09aec557fe474c717
Ali <ali.boushehri@roche.com>
2022-08-02 09:43:59 +0200

    added random_state instead of hard coding

--- bright2nuc/__init__.py
+++ bright2nuc/__init__.py
@@ -1,15 +1,15 @@
 from bright2nuc.utils import *
 from bright2nuc.segmentation.Regression import Regression
 
-def GetPipeLine(configs):
+def GetPipeLine(configs, random_state=123):
     if "seeds" in configs:
         if configs["seeds"] == True:
             import numpy as np
-            np.random.seed(123)
+            np.random.seed(random_state)
             import random as python_random
-            python_random.seed(123)
+            python_random.seed(random_state)
             import tensorflow as tf
-            tf.random.set_random_seed(123)
+            tf.random.set_random_seed(random_state)
             sess = tf.Session(graph=tf.get_default_graph())
             K.set_session(sess)
     configs.update({"use_algorithm": "Regression"})
